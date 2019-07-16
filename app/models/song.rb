58ff145071ec11495d734545519655777446929f
Iskra B <ibatistapoblete@gmail.com>
2019-07-16 03:54:33 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -7,6 +7,7 @@ class Song < ActiveRecord::Base
   end
 
   def drake_made_this
+    binding.pry
     drake = self.artist
     # when this method is called it should assign the song's artist to Drake
   end
