da8fc372fb80273528f792844a4daf8c5e79c6bf
Iskra B <ibatistapoblete@gmail.com>
2019-07-16 03:48:50 +0000

    Automatically backed up by Learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -14,7 +14,7 @@ class Genre < ActiveRecord::Base
 
   def all_artist_names
     self.artists do |artist|
-      artist.name
+      self.artist.name
     end
     # return an array of strings containing every musician's name
   end
